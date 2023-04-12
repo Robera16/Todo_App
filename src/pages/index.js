f6605064b41078e329b0ac86a41d9c00c138ec9a
Robera16 <roberaworknehrobwork@gmail.com>
2023-04-12 22:39:53 +0300

    added navbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,15 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+// import Head from 'next/head'
+// import Image from 'next/image'
+// import { Inter } from 'next/font/google'
+// import styles from '@/styles/Home.module.css'
+// const inter = Inter({ subsets: ['latin'] })
 
-const inter = Inter({ subsets: ['latin'] })
+import Navbar from '../components/Navbar'
 
 export default function Home() {
   return (
     <>
+      <Navbar />
       <h1> HomePage</h1>
     </>
   )
