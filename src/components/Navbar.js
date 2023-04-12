da1493e7cdcf59645bebabd93cd59149f7b2cd10
Robera16 <roberaworknehrobwork@gmail.com>
2023-04-13 00:24:56 +0300

    added login page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,37 @@
+import Link from "next/link"
+import styles from './Navbar.module.css'
+
 export default function Home() {
+    const {user} = 'userA'
     return (
-      <>
-        <h1>Navbar</h1>
-      </>
+        <nav className={styles.navbar}>
+        <ul >
+            <li className={styles.title}><Link href="/">TODO</Link></li>
+            
+            {!user && (
+              <>
+                <li>
+                    <Link href="/login">
+                        Login
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/signup">
+                        Signup
+                    </Link>
+                </li>
+              </>
+            )}
+
+            {user && (
+              <>
+                <li>hello, {user}</li>
+                <li>
+                  <button className='btn' onClick={logout}>Logout</button>
+                </li>
+              </>
+            )}
+        </ul>
+    </nav>
     )
   }
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,9 @@
 // import styles from '@/styles/Home.module.css'
 // const inter = Inter({ subsets: ['latin'] })
 
-import Navbar from '../components/Navbar'
-
 export default function Home() {
   return (
     <>
-      <Navbar />
       <h1> HomePage</h1>
     </>
   )
